6a5ee53bff4a56d7e2121613fe13b776050dd23b
Franck Garcia <franck_garcia@ftn.fedex.com>
2019-10-01 09:47:22 -0400

    Log greeting call

--- src/main/java/edu/ca/ets/Greeter.java
+++ src/main/java/edu/ca/ets/Greeter.java
@@ -9,6 +9,7 @@ import static java.lang.String.format;
 public class Greeter 
 {
     public String greet(String name) {
+	System.out.println("greet was called with param" + name);
         return format("Greetings %s! How are you doing today?", name);
     }
 }
