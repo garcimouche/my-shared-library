cf26bc4d09a5486ca049f0caae9f73f862b82274
Franck Garcia <franck_garcia@ftn.fedex.com>
2019-10-01 08:19:52 -0400

    shared library

---
+++ src/main/java/edu/ca/ets/Greeter.java
@@ -0,0 +1,14 @@
+package edu.ca.ets;
+
+import static java.lang.String.format;
+
+/**
+ * Hello world!
+ *
+ */
+public class Greeter 
+{
+    public String greet(String name) {
+        return format("Greetings %s! How are you doing today?", name);
+    }
+}
