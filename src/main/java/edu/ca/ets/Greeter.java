6b4000b0a239199f415c03634cd96739eb41adf4
Franck Garcia <franck_garcia@ftn.fedex.com>
2019-10-01 10:30:18 -0400

    add space

--- src/main/java/edu/ca/ets/Greeter.java
+++ src/main/java/edu/ca/ets/Greeter.java
@@ -9,7 +9,7 @@ import static java.lang.String.format;
 public class Greeter 
 {
     public String greet(String name) {
-	System.out.println("greet was called with param " + name);
-        return format("Greetings %s!How are you doing today?", name);
+	System.out.println("Greeter.greet() was called with param " + name);
+        return format("Greetings %s! How are you doing today?", name);
     }
 }
